608d6d70b42e7aa44ddf41fa015ce552a770c16c
Nobuyoshi Tada <nobuyoshi.tada@tandemworks.co.jp>
2026-01-26 10:36:21 +0900

    Fix: configure Amplify before importing App

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+// 先に必ず設定する
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// configure の後に App を読み込む（ここが肝）
+import("./App.tsx").then(({ default: App }) => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
