3975bf29606e6dcd3cf2f496e224065435b60d90
Nobuyoshi Tada <nobuyoshi.tada@tandemworks.co.jp>
2026-03-05 14:22:56 +0900

    Revert auth userAttributes change to keep existing UserPool

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,30 +7,5 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    // phone: true, // ←後で体験したい時にON（ONにすると phoneNumber が必須扱いになります）
   },
-
-  // サインアップ時に持たせたい属性
- userAttributes: {
-  // Cognito標準属性 'name' は、ここでは fullname として指定します
-  fullname: {
-    required: false,
-    mutable: true,
-  },
-  preferredUsername: {
-    required: false,
-    mutable: true,
-  },
-
-  "custom:organization": {
-    dataType: "String",
-    mutable: true,
-  },
-},
-
-  // MFA：まずは TOTP（認証アプリ）をON
-  multifactor: {
-  mode: 'REQUIRED',
-  totp: true,
-},
 });
